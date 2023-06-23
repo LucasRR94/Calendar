5feb398a4a97d9cfca10c2d18edab2d536b84608
Lucas Rocha <lucasregodarocha@outlook.com>
2023-06-22 22:34:36 -0300

    feat: created constants file in components/ui

---
+++ src/components/ui/constants.ts
@@ -0,0 +1,23 @@
+export const nullYear: Number = 0;
+
+export const UPDATE_DATE_SELECTOR_EVENT = "updateSelectorYearOrMonth";
+
+export const DISABLE_SELECTOR_VISIBILITY = "disableSelector";
+
+export const DISABLE_POPOVER_VISIBILITY = "disable_popover_visibility";
+
+export const UPDATE_DATA_PICKER = "updateDataPicker";
+
+export const UPDATE_CALENDAR_MONTH = "updateCalendarMonth";
+
+export const UPDATE_CALENDAR_YEAR = "updateCalendarYear";
+
+export const HANDLE_CLICK_UP = "handleClickUp";
+
+export const HANDLE_CLICK_DOWN = "handleClickDown";
+
+export const HANDLE_INPUT_TEXT = "handleInputText";
+
+export const UPDATE_CALENDAR_YEAR_MONTH = "updateCalendarYearMonth";
+
+export const DAY_SELECTED = "daySelected";

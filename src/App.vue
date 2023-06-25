b26100cec50ddb4adaf3c344bedba4112291d147
Lucas Rocha <lucasregodarocha@outlook.com>
2023-06-25 15:33:13 -0300

    refactor: refactored App

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,27 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+  <div class="app"><Calendar /></div>
 </template>
-  
+<script lang="ts">
+import Vue from "vue";
+import Calendar from "@/components/Calendar/Calendar.vue";
+export default Vue.extend({
+  components: { Calendar },
+});
+</script>
 <style lang="scss">
-  html,*,body{
-    margin: 0 0;
-    padding: 0 0;
-    box-sizing: border-box;
-  }
+html,
+*,
+body {
+  margin: 0 0;
+  padding: 0 0;
+  box-sizing: border-box;
+}
+body {
+  width: 100vw;
+  height: 100vh;
+}
+.app {
+  width: 100%;
+  height: 100%;
+}
 </style>

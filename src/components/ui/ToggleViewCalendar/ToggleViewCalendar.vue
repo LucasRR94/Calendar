30863816a0da5f2969c15264e0a28c12ab227b77
Lucas Rocha <lucasregodarocha@gmail.com>
2024-01-09 22:00:47 -0300

    feat: created the ToggleViewCalendar in ui

---
+++ src/components/ui/ToggleViewCalendar/ToggleViewCalendar.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="toogle">
+    <span :class="{ disable: isMonthView }">Month</span>
+    <label class="switch">
+      <input type="checkbox" :value="isMonthView" @click="toggleState" />
+      <span class="slider round"></span>
+    </label>
+    <span :class="{ disable: !isMonthView }">Year</span>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue, { PropType } from "vue";
+import { CHANGE_TOGGLE_VIEW } from "../constants";
+
+export default Vue.extend({
+  name: "ToggleViewCalendar",
+  props: {
+    isMonthView: {
+      type: Boolean as PropType<boolean>,
+      required: true,
+    },
+  },
+  methods: {
+    toggleState: function () {
+      this.$emit(CHANGE_TOGGLE_VIEW);
+    },
+  },
+});
+</script>
+
+<style lang="scss">
+@import "../../../style/ToggleViewCalendar/ToggleViewCalendar";
+</style>
